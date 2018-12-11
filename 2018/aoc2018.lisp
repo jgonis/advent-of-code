a862aabce93ce9563c62efd67a9b21ade55ea987
Jeff Gonis <jeffgonis@fastmail.com>
2018-12-10 22:13:00 -0700

    solve problem 3-2
    Refactor p3-1 as well so that I could reuse most of the code for 3-2

--- 2018/aoc2018.lisp
+++ 2018/aoc2018.lisp
@@ -19,6 +19,7 @@
       (setf current-freq (+ offset current-freq))
       (multiple-value-bind (value present) (gethash current-freq 
                                                     calculated-freqs)
+        (declare (ignore value))
         (cond (present (return current-freq))
               (t (setf (gethash current-freq 
                                 calculated-freqs) 
@@ -50,6 +51,7 @@
 (defun contains-char-count (letter-hash count)
   (with-hash-table-iterator (next letter-hash)
     (loop (multiple-value-bind (more? key value) (next)
+            (declare (ignore key))
             (unless more? (return '()))
             (if (= value count)
                 (return t))))))
@@ -101,24 +103,27 @@
         (coord-hash (make-hash-table :test 'equal))
         (contested-count 0))
     (dolist (line input)
-      (parse-p3 line coord-hash))
+      (parse-p3 line coord-hash #'fabric-coord-func))
     (with-hash-table-iterator (next coord-hash) 
       (loop (multiple-value-bind (more? key value) (next)
               (unless more? (return '()))
-              (if (>= value 2)
+              (if (and (consp key) (>= value 2))
                   (setf contested-count (+ contested-count 1))))))
     contested-count))
 
-(defun parse-p3 (line coord-hash)
-  (let* ((start-coords-string (aoc-utils:trim-spaces (subseq line 
-                                                     (+ 1 (position #\@ line))
-                                                     (position #\: line))))
+(defun parse-p3 (line coord-hash set-fabric-coord-func)
+  (let* ((claim-id (aoc-utils:trim-spaces (subseq line 
+                                                  (+ 1 (position #\# line)) 
+                                                  (position #\Space line))))
+         (start-coords-string (aoc-utils:trim-spaces (subseq line 
+                                                             (+ 1 (position #\@ line))
+                                                             (position #\: line))))
          (start-size-string (aoc-utils:trim-spaces (subseq line
                                                            (+ 1 (position #\: line))
                                                            (length line))))
          (start-x (parse-integer (subseq start-coords-string 
-                                                    0 
-                                                    (position #\, start-coords-string))))
+                                         0 
+                                         (position #\, start-coords-string))))
          (start-y (parse-integer (subseq start-coords-string 
                                          (+ 1 (position #\, start-coords-string))
                                          (length start-coords-string))))
@@ -128,20 +133,47 @@
          (height (parse-integer (subseq start-size-string 
                                         (+ 1 (position #\x start-size-string)) 
                                         (length start-size-string)))))
+    (setf (gethash claim-id coord-hash) t)
     (dotimes (x width)
       (dotimes (y height)
-        (multiple-value-bind (value present?) (gethash (cons (+ x start-x) 
-                                                             (+ y start-y)) 
-                                                       coord-hash)
-          (cond ((null present?) (setf (gethash (cons (+ x start-x) 
-                                                      (+ y start-y)) 
-                                                coord-hash) 
-                                       1))
-                (t (setf (gethash (cons (+ x start-x) 
-                                        (+ y start-y)) 
-                                  coord-hash) 
-                         (+ value 1)))))))))
+        (let ((x-coord (+ x start-x))
+              (y-coord (+ y start-y)))
+          (multiple-value-bind (value present?) (gethash (cons x-coord 
+                                                               y-coord) 
+                                                         coord-hash)
+            (funcall set-fabric-coord-func x-coord y-coord claim-id value present? coord-hash)))))))
+
+(defun fabric-coord-func (x-coord y-coord claim-id value present? coord-hash)
+  (declare (ignore claim-id))
+  (cond ((null present?) (setf (gethash (cons x-coord 
+                                              y-coord) 
+                                        coord-hash) 
+                               1))
+        (t (setf (gethash (cons x-coord 
+                                y-coord) 
+                          coord-hash) 
+                 (+ value 1)))))
+
+(defun fabric-id-coord-func (x-coord y-coord claim-id value present? coord-hash)
+    (cond ((null present?) (setf (gethash (cons x-coord 
+                                              y-coord) 
+                                        coord-hash) 
+                               (cons claim-id 1)))
+        (t (setf (gethash (cons x-coord 
+                                y-coord) 
+                          coord-hash) 
+                 (cons (car value) (+ (cdr  value) 1)))
+           (setf (gethash (car value) coord-hash) nil)
+           (setf (gethash claim-id coord-hash) nil))))
 
 (defun problem3-2 (input-path)
-  (let ((input (aoc-utils:input->list input-path)))))
+  (let ((input (aoc-utils:input->list input-path))
+        (coord-hash (make-hash-table :test 'equal)))
+    (dolist (line input)
+      (parse-p3 line coord-hash #'fabric-id-coord-func))
+    (with-hash-table-iterator (next coord-hash) 
+      (loop (multiple-value-bind (more? key value) (next)
+              (unless more? (return '()))
+              (if (and (stringp key) value)
+                  (format t "~A~%" key)))))))
   
