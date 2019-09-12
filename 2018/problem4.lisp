b53a80cb7e49d918c1acac897b9b5f7b35e39939
jeff <jeffgonis@fastmail.com>
2019-09-12 16:33:36 -0600

    Begin work on using state machine for parsing.

--- 2018/problem4.lisp
+++ 2018/problem4.lisp
@@ -15,6 +15,36 @@
              (chunked-input (chunk-input sorted-lines)))
         )))
 
+(defclass shift-record-state ()
+  ((guard-number :accessor guard-number)
+   (shift-date :accessor shift-date)
+   (asleep-awake-log :accessor asleep-awake-log)))
+(defclass initial-state ())
+(defclass guard-state (shift-record-state))
+(defclass asleep-state (shift-record-state))
+(defclass awake-state (shift-record-state))
+
+(defclass p4-input ()
+  ((input-line :initarg :line 
+               :accessor input-line)))
+(defclass guard-input (p4-input))
+(defclass fall-asleep-input (p4-input))
+(defclass wake-up-input (p4-input))
+(defclass eof-input ())
+
+(defun make-input (input-line)
+  (cond ((search "Guard" input-line) (make-instance 'guard-input 
+                                                    :line input-line))
+        ((search "falls" input-line) (make-instance 'fall-asleep-input 
+                                                    :line input-line))
+        ((search "wakes" input-line) (make-instance 'wake-up-input
+                                                    :line input-line))
+        (t ())))
+
+(defgeneric handle-input (state input-line computed-shift-collection))
+(defmethod handle-input ((state initial-state) input-line computed-shift-collection)
+  (let ))
+
 (defun make-p4-record (shift-start-line)
   (let* ((date-string (subseq shift-start-line 1 (position #\] shift-start-line)))
          (date-time (aoc-utils:make-date-time date-string))
